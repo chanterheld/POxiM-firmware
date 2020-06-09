89fad525740b385521f4f68d22dda6d8656cafef
Daan <chanterheld@gmail.com>
2020-06-09 13:30:19 +0200

    update filters to mem saving on decimation

--- Src/stage1_filter.c
+++ Src/stage1_filter.c
@@ -24,25 +24,25 @@
 #define filter_input_type		int8_t
 #define filter_output_type		int16_t
 
-FILTER_MEMORY(FILTER_NAME, filter_output_type, FILTER_SIZE);
 
+FILTER_MEMORY_MEM_SAVE(FILTER_NAME, filter_output_type, FILTER_SIZE);
 
 DEC_FIR_FILTER_ADVANCE(FILTER_NAME, filter_input_type, filter_output_type){
 
 	if(output_cycle == 1){
 
-		FILTER_FIRST_LINE(FILTER_NAME, filter_output_type);
-		FILTER_LINE(FILTER_NAME, 2);
-		FILTER_LINE(FILTER_NAME, 4);
+		FILTER_FIRST_LINE_MEM_SAVE(FILTER_NAME, filter_output_type);
+		FILTER_LINE_MEM_SAVE_MOVE(FILTER_NAME, 2);
+		FILTER_LINE_MEM_SAVE_MOVE(FILTER_NAME, 4);
 
 		/*this line might have to go to the other block depending on filter size(even/uneven), the */
-		FILTER_LAST_LINE(FILTER_NAME, 6);
+		FILTER_LAST_LINE_MEM_SAVE_EVEN(FILTER_NAME, 6);
 
 	    return retval;
 	}else{
-		FILTER_LINE(FILTER_NAME, 1);
-		FILTER_LINE(FILTER_NAME, 3);
-		FILTER_LINE(FILTER_NAME, 5);
+		FILTER_LINE_MEM_SAVE_STATIONARY(FILTER_NAME, 1);
+		FILTER_LINE_MEM_SAVE_STATIONARY(FILTER_NAME, 3);
+		FILTER_LINE_MEM_SAVE_STATIONARY(FILTER_NAME, 5);
 	    return 0;
 	}
 
